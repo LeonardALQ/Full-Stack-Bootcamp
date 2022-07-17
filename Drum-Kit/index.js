fff1491f876420061e133521dc5f5bbe545fabb7
Leonard <leonard.alq@gmail.com>
2022-07-17 14:47:27 +1000

    Drum-Kit Completed

---
+++ Drum-Kit/index.js
@@ -0,0 +1,41 @@
+let drums = document.querySelectorAll(".drum");
+let audio;
+for (let i = 0; i < drums.length; i++) { 
+  drums[i].addEventListener("click", drumClicked);
+}
+
+document.addEventListener("keydown", drumPressed);
+function drumClicked() { 
+  playDrum(this.textContent);
+}
+
+function drumPressed() { 
+  playDrum(event.key);
+}
+
+function playDrum(key) { 
+  switch(key) {
+    case "w":
+      audio = new Audio("sounds/tom-1.mp3");
+      break;
+    case "a":
+      audio = new Audio("sounds/tom-2.mp3");
+      break;
+    case "s":
+      audio = new Audio("sounds/tom-3.mp3");
+      break;
+    case "d":
+      audio = new Audio("sounds/tom-4.mp3");
+      break;
+    case "j":
+      audio = new Audio("sounds/snare.mp3");
+      break;
+    case "k":
+      audio = new Audio("sounds/crash.mp3");
+      break;
+    case "l":
+      audio = new Audio("sounds/kick-bass.mp3");
+      break;
+  } 
+  audio.play();
+}
